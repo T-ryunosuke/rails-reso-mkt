6c3aab4ce3045e9ee3ae8a233b48c03a708854db
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-01-18 18:24:00 +0900

    フラッシュメッセージ機能追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,20 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        flashFade: {
+          "0%": { transform: "translateY(0px)", opacity: 1 },
+          "70%": { transform: "translateY(0)", opacity: 1 },
+          "90%": { transform: "translateY(-8px)", opacity: 0 },
+          "100%": { transform: "translateY(-30px)", opacity: 0 }
+        }
+      },
+      animation: {
+        flash: "flashFade 4.0s forwards"
+      }
+    }
+  }
 }
