92a9304bd1da887767b3407b6839d69daa1be5ec
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-02-13 21:59:19 +0900

    知りたい価格情報を示すための機能追加

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 ENV.each { |k, v| env(k, v) }
 
 # 環境を指定（デフォルトは development に）
-rails_env = ENV['RAILS_ENV'] || 'development'
+rails_env = ENV["RAILS_ENV"] || "development"
 set :environment, rails_env
 
 # ログの出力先
